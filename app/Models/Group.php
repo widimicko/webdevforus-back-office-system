722d9d0a1f02ab6ea8ee7ee7c497d9cc5b434afb
Micko Widi <widimicko85@gmail.com>
2022-07-25 19:07:04 +0700

    Create User, Member, Group Model

---
+++ app/Models/Group.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Member;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Group extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'groupid';
+    protected $protected = ['groupid'];
+
+    public function members() : HasMany
+    {
+        return $this->hasMany(Member::class, 'groupid');
+    }
+}
---
+++ app/Models/Member.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Group;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Member extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'memberid';
+    protected $protected = ['memberid'];
+
+    public function group() : BelongsTo
+    {
+        return $this->belongsTo(Group::class, 'groupid');
+    }
+}
