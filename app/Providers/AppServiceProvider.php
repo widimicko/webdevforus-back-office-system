b59a328facfed1e13bf0776e46403418c65f4917
Micko Widi <widimicko85@gmail.com>
2022-07-25 18:33:06 +0700

    create gate & middleware (hak akses)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('Admin', function(User $user) {
+            return $user->role == 'Admin';
+        });
+
+        Gate::define('Member', function(User $user) {
+            return $user->role == 'Member';
+        });
     }
 }
